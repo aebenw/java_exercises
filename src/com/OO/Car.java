c5ce62273dd94af97191557b8a1000e91ce86282
github username <github email address>
2018-12-12 15:32:47 -0500

    composition of relationships

---
+++ src/com/OO/Car.java
@@ -0,0 +1,11 @@
+package com.OO;
+
+public class Car extends Vehicle{
+
+    private String name;
+
+    public Car(int gears, String name) {
+        super(gears);
+        this.name = name;
+    }
+}
---
+++ src/com/OO/Vehicle.java
@@ -0,0 +1,14 @@
+package com.OO;
+
+public class Vehicle {
+
+    private int gears;
+
+    public Vehicle(){
+        this(4);
+    }
+
+    public Vehicle(int gears) {
+        this.gears = gears;
+    }
+}
