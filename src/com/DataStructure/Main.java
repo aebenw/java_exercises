7e966a3c85f31a5dee636bdfe118c1a192b5fc5e
github username <github email address>
2018-12-17 16:40:43 -0500

    arraylist funcitons

--- src/com/DataStructure/Main.java
+++ src/com/DataStructure/Main.java
@@ -1,9 +1,34 @@
 package com.DataStructure;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList gL = new GroceryList();
 
+    public static void main(String[] args){
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+
+
+
+    }
+
+    private static void printInstructions(){
+        System.out.println("instructions");
+    }
+
+    public static void processArrayList(){
+        //how to copy an arraylist into a new arraylist
+        ArrayList<String> newArray = new ArrayList<String>();
+        newArray.addAll(gL.getGroceryList());
+
+        //how to turn an arrayList into an Array
+        String[] myArray = new String[gL.getGroceryList().size()];
+        myArray = gL.getGroceryList().toArray(myArray);
     }
 
 }
